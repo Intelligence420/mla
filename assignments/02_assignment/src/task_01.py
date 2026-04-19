e715199435d612e7c1a37ba8ba9748d371102786
Moritz <dermoritzmartin@gmail.com>
2026-04-19 13:19:05 +0200

    Einheiten task1

--- assignments/02_assignment/src/task_01.py
+++ assignments/02_assignment/src/task_01.py
@@ -21,8 +21,12 @@ def report_device_properties():
     for key, value in cp.cuda.Device().attributes.items():
         if key in keys_of_interest:
             print(f"{key}: {value}")
+    print(f"Einheiten sind: L2CacheSize, MaxSharedMemoryPerMultiprocessor → Bytes, ClockRate → kHz")
     print(f"========================================")
 
+    # aus gründen, kann .items nicht ordnetlich mit argumenten gefüttert werden
+    # sodass es immer alle: (key, value)-Paare --> https://docs.python.org/3/library/stdtypes.html#dict.items
+    # zurückgibt. Also muss ich danach filtern und dann printen
 
 if __name__ == "__main__":
     report_device_properties()
